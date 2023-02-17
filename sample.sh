ac7528d4e50fe559480a1cd8e4adb4cd1c296662
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-02-17 22:10:08 +0530

    Sample calculator

---
+++ sample.sh
@@ -0,0 +1,26 @@
+#How to create calculator using shell script
+#!/bin/bash
+
+echo "Enter the first number:"
+read num1
+
+echo "Enter the second number:"
+read num2
+
+echo "Choose an operation (+, -, *, /):"
+read op
+
+if [ "$op" == "+" ]; then
+  result=$((num1 + num2))
+elif [ "$op" == "-" ]; then
+  result=$((num1 - num2))
+elif [ "$op" == "*" ]; then
+  result=$((num1 * num2))
+elif [ "$op" == "/" ]; then
+  result=$((num1 / num2))
+else
+  echo "Invalid operator."
+  exit 1
+fi
+
+echo "Result: $result"
