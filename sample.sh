3b618f4b6d9a52d81852a7213205460668d4e9c5
geddadalakshmanrao <lakshmangeddada1994@gmail.com>
2023-03-16 22:41:27 +0530

    updated

--- sample.sh
+++ sample.sh
@@ -1,31 +1,5 @@
-#How to create calculator using shell script
-#!/bin/bash
-#installing git in server
-sudo yum install git -y
-git --version
-
-
-#calculator
-echo "Enter the first number:"
-read num1
-
-echo "Enter the second number:"
-read num2
-
-echo "Choose an operation (+, -, *, /):"
-read op
-
-if [ "$op" == "+" ]; then
-  result=$((num1 + num2))
-elif [ "$op" == "-" ]; then
-  result=$((num1 - num2))
-elif [ "$op" == "*" ]; then
-  result=$((num1 * num2))
-elif [ "$op" == "/" ]; then
-  result=$((num1 / num2))
-else
-  echo "Invalid operator."
-  exit 1
-fi
-
-echo "Result: $result"
+# Hello World
+# ls
+ls
+ls -l
+ls -l /opt
